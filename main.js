3d9b58e8e29e86a876c0e1b31d747f3476711b96
Harshad Prajapati <89201259+harshad00@users.noreply.github.com>
2023-07-29 21:42:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// //box button
+// let btn2 = document.querySelector('chatbox-btn2')
+// btn2 = document.addEventListener('click', func1)
+
+// function func1() {
+//     console.log('hello')
+//     document.location.href = "box-button]/box.html"
+// }
+/* js for search ber */
+
+
+
+
+function fun1() {
+
+    // document.querySelector('chatbox-btn2').click;
+    document.location.href = "box-button]/box.html"
+    console.log('hello')
+
+}
